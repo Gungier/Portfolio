44927a3e8a6513ed9b04e479155f46be863dbb45
Gabe <gungier1@gmail.com>
2019-07-30 18:41:11 -0400

    moved to root

---
+++ precache-manifest.161e22f4cc9bef91214274d04e456361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191581859e8628521be16da1a0f64498",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a2ac0b302faa626a60ed",
+    "url": "/Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a2ac0b302faa626a60ed",
+    "url": "/Portfolio/static/js/main.f6665052.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
